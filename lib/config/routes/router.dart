19d89778542f4ae40f1d3ee57ef38228fa0fff15
mohammadzamaniaf <132180413+CoderrHQ@users.noreply.github.com>
2023-09-02 16:14:07 +0430

    starter code for the tutorial

---
+++ lib/config/routes/router.dart
@@ -0,0 +1,21 @@
+import 'package:facebook_clone/core/screens/error_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+class Router {
+  static Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      default:
+        return _cupertinoRoute(
+          ErrorScreen(
+            error: 'Wrong Route provided ${settings.name}',
+          ),
+        );
+    }
+  }
+
+  static Route _cupertinoRoute(Widget view) => CupertinoPageRoute(
+        builder: (_) => view,
+      );
+
+  Router._();
+}
