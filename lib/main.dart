0fee3426c4abe9222a9f16c6fe51dd5498246456
mohammadzamaniaf <132180413+CoderrHQ@users.noreply.github.com>
2023-09-02 15:42:03 +0430

    test commit!

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Hello World'),
         ),
       ),
     );
